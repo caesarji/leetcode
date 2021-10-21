af5342c6c861c5fb3760c570073ba7f435f5773a
Siddhant Gupta <siddhantgupta384@gmail.com>
2021-10-22 00:28:47 +0530

    tree cousin keeping track of parent

---
+++ 993.cousins-in-binary-tree.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=993 lang=cpp
+ *
+ * [993] Cousins in Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int find_HeightParent(TreeNode *curr,int &parent,int value,int height)
+    {
+        if(!curr)
+            return 0;
+        if(curr->val==value)
+            return height;
+        
+        parent = curr->val;
+        int left = find_HeightParent(curr->left,parent,value,height+1);
+        if(left)
+            return left;
+        parent = curr->val;
+        int right = find_HeightParent(curr->right,parent,value,height+1);
+        return right;
+    }
+public:
+    bool isCousins(TreeNode* root, int x, int y) {
+        if(root->val==x || root->val==y)
+            return false;
+            
+        int parent=-1;
+        int xHeight = find_HeightParent(root,parent,x,0);
+        
+        int yParent=-1;
+        int yHeight = find_HeightParent(root,yParent,y,0);
+        
+        if(parent!=yParent && xHeight==yHeight)
+            return true;        
+        return false;
+    }
+};
+// @lc code=end
+
